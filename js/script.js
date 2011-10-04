b71d3cff3b2615e8ebbbb04516ecf76b5ad95083
Marques Woodson <marques.woodson@gmail.com>
2011-10-03 23:39:02 -0500

    all changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 /* Author: 
 
 */
-
+$(function(){
+	var slid = $("#slider-ul li");
+	slid.click(function(){
+		var path = "img/" + $(this).data("name");
+		$("#container").fadeOut(300, function(){
+			$(this).css("background","url("+path+")").fadeIn(300);
+		});
+			
+	});
+	
+		
+})
 
 
 
