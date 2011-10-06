9a39f6a4ecca883cede3f43d2a666bf940e8a4e0
Marques Woodson <marques.woodson@gmail.com>
2011-10-06 15:50:50 -0500

    latest changes

--- js/script.js
+++ js/script.js
@@ -9,15 +9,40 @@ $(function(){
 			$(this).css("background","url("+path+")").fadeIn(300);
 		});
 		
-		$(this).children("img")
+		/*$(this).children("img")
 		.slideUp(300);
 		$(this)
 		.children("p")
 		.fadeIn(300);
 		
-			
+			*/
+	});
+		
+	$("#showSide").live("click", function(){
+		$("#sidebar").animate({
+			left: "0px"
+		}, 300);
+		
+		$("#container, footer").animate({
+			left:"300px"
+		}, 300);
+		
+		
+		$(this).attr("id", "closeSide").html("Close Sidebar");
+		
 	});
 	
+	$("#closeSide").live("click", function(){
+		$("#sidebar").animate({
+			left: "-300px"
+		}, 300);
+		
+		$("#container, footer").animate({
+			left:"0px"
+		}, 300);
+		
+		$(this).attr("id", "showSide").html("Show Sidebar");
+	})
 		
 })
 
